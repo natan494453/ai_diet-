3770a243cb5860acdfb0af7f0482455be7a5edaf
henrix494 <henrix494@gmail.com>
2024-04-21 12:19:17 +0300

    change local

--- app/api/completion/route.ts
+++ app/api/completion/route.ts
@@ -4,7 +4,6 @@ import { GoogleGenerativeAIStream, StreamingTextResponse } from "ai";
 const genAI = new GoogleGenerativeAI(process.env.GOOGLE_API_KEY || "");
 
 // IMPORTANT! Set the runtime to edge
-export const runtime = "edge";
 
 export async function POST(req: Request) {
   // Extract the `prompt` from the body of the request
