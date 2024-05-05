50fd10de72743e2900c76e566241fc3e82dd10e7
henrix494 <henrix494@gmail.com>
2024-05-05 19:50:13 +0300

    added prefoix

--- postcss.config.mjs
+++ postcss.config.mjs
@@ -1,8 +1,10 @@
+import autoprefixer from "autoprefixer";
 import daisyui from "daisyui";
 /** @type {import('postcss-load-config').Config} */
 const config = {
   plugins: {
     tailwindcss: {},
+    autoprefixer: {},
   },
 };
 
