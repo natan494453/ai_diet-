873918391d2bb6bfb44ad9055664bdd251dcfa9d
henrix494 <henrix494@gmail.com>
2024-05-06 13:56:21 +0300

    fix try now btn

--- app/sign-in/[[...sign-in]]/page.tsx
+++ app/sign-in/[[...sign-in]]/page.tsx
@@ -3,7 +3,7 @@ import { SignIn } from "@clerk/nextjs";
 export default function Page() {
   return (
     <div className="w-screen flex justify-center h-[80vh] items-center">
-      <SignIn path="/sign-in" />
+      <SignIn forceRedirectUrl={"/dashboard"} path="/sign-in" />
     </div>
   );
 }
